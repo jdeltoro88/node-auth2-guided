2f8a7789a6678b3fef5f8c5ef94c7448bcdb4204
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-23 18:33:36 +0100

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -7,8 +7,10 @@ module.exports = (req, res, next) => {
   if (!token) {
     res.status(401).json({ message: 'you shall not pass' })
   } else {
-    jwt.verify(token, jwtSecret, (err, de) => {
-
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ message: 'token bad' })
+      }
     })
   }
 };
