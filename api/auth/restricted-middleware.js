0317ac46ad056410c2df1ef3d6b3a3d50cb264c4
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-23 18:31:13 +0100

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,9 +1,12 @@
+const jwt = require('jsonwebtoken')
+const {} = require('../../config/secrets')
+
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
 
   if (!token) {
     res.status(401).json({ message: 'you shall not pass' })
   } else {
-    
+
   }
 };
