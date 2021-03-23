9d9107861da7c30b93961dd9f879844b634cc05c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-23 18:58:02 +0100

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -4,7 +4,9 @@ const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
 
 const only = role => (req, res, next) => {
-  // 
+  // fish the token from req
+  // fish the actual role from the token
+  // compare that against the role coming in through args
 }
 
 router.get("/", restricted, only('admin'), (req, res) => {
